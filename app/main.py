48d2fe0174109260ec4fa9f16c9d194c19afaab8
Imane Merghichi <47861134+imerghichi@users.noreply.github.com>
2022-06-29 17:03:03 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,25 +1,9 @@
-from flask import Flask, render_template, request, redirect
-import pyttsx3
-from talkative.app.textForm import TextForm
-import os
+from flask import Flask
 
 app = Flask(__name__, template_folder='templates')
 SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
-@app.route('/')
-def hello_world():
-    form = TextForm()
-    return render_template("hello.html", form = form)
-
-@app.route('/', methods=['POST'])
-def my_form_post():
-    text = request.form['textTospeech']
-    friend = pyttsx3.init()
-    friend.say(text)
-    friend.runAndWait()
-    friend.startLoop(False)
-    return redirect('/')
 @app.route('/deliverActCode', methods=['POST'])
 def deliverActCode():
     return "test"
