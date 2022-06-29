b27cb890d0b66b961f51f5c8c7b74707234b98d9
Imane Merghichi <47861134+imerghichi@users.noreply.github.com>
2022-06-29 17:03:21 +0100

    Delete textForm.py

--- app/textForm.py
+++
@@ -1,6 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import  DataRequired
-class TextForm(FlaskForm):
-    textTospeech = StringField('ENTRER LE TEXT QUE VOUS VOULEZ ENTENDRE', validators=[DataRequired()])
-    submit = SubmitField('ENTENDRE')
